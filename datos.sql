bc7f0bf192f254b9bf24966c920ba0a45109d879
alesvolta <56805037+Voltios@users.noreply.github.com>
2025-04-16 12:09:00 +0200

    Update datos.sql

--- datos.sql
+++ datos.sql
@@ -1,117 +1,36 @@
+/*
+        Here you should add the content of an initial database load (inserts and that stuff)
+
 insert INTO programas
 values (
-        null,
-        "Gent amb trellat",
-        "Un podcast de Nerea Pedron i Gema Dalia.",
+        null, /* auto increment */
+        "name",
+        "Description",
         1,
-        "https://open.spotify.com/show/3xKLNISLH2jtZ10smgCTmz",
-        0,
-        CURRENT_TIMESTAMP,
-        CURRENT_TIMESTAMP
-    ),
-    (
-        null,
-        "DesVARiando",
-        "Bienvenidos a DesVARiando, un programa en el que se analiza el futbol de manera rigurosa, o al igual no tanto....",
-        2,
-        "https://open.spotify.com/show/0XctocTbaR5tHbDcJAJCmf",
-        0,
-        CURRENT_TIMESTAMP,
-        CURRENT_TIMESTAMP
-    ),
-    (
-        null,
-        "Entiende tu mente",
-        "Entiende Tu Mente es un podcast de psicologia que, de una manera amena, te cuenta como funciona nuestra mente.",
-        3,
-        "https://open.spotify.com/show/0sGGLIDnnijRPLef7InllD?si=b79e09b8c8df434a",
+        "link",
         0,
         CURRENT_TIMESTAMP,
         CURRENT_TIMESTAMP
     );
+
 insert into categorias
 values (
         null,
-        "deportes",
-        CURRENT_TIMESTAMP,
-        CURRENT_TIMESTAMP
-    ),
-    (
-        null,
-        "actualidad",
-        CURRENT_TIMESTAMP,
-        CURRENT_TIMESTAMP
-    ),
-    (
-        null,
-        "musica",
-        CURRENT_TIMESTAMP,
-        CURRENT_TIMESTAMP
-    ),
-    (
-        null,
-        "videojuegos",
-        CURRENT_TIMESTAMP,
-        CURRENT_TIMESTAMP
-    ),
-    (
-        null,
-        "tecnologia",
-        CURRENT_TIMESTAMP,
-        CURRENT_TIMESTAMP
-    ),
-    (
-        null,
-        'salud',
-        CURRENT_TIMESTAMP,
-        CURRENT_TIMESTAMP
-    ),
-    (
-        null,
-        'talk-show',
+        "sports",
         CURRENT_TIMESTAMP,
         CURRENT_TIMESTAMP
     );
 insert into programa_categoria
-values (null, 1, 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-    (null, 2, 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-    (null, 2, 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-    (null, 3, 4, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-    (null, 3, 5, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+values (null, 1, 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
 insert into episodios
 VALUES(
         null,
-        "Capitol 01/T1 - Fill predilecte",
-        "https://open.spotify.com/episode/5XY3Qg3vONCrU6NWe86fH0",
-        "Holiiii, som Gema i Nerea. Vos donem la benvingudissima al nostre primer capitol d'aquest podcast que hem creat amb moltissima il.lusió, un lloc on desconnectar, on riure i on reivindicar-nos. Esperem que ho gaudiu tant com nosaltres!!",
+        "Chapter 1/01",
+        "link",
+        "Description",
         1,
-        CURRENT_TIMESTAMP,
-        CURRENT_TIMESTAMP
-    ),
-    (
-        null,
-        "Capitol 02/T1 - Teletransport involuntari",
-        "https://open.spotify.com/episode/5KxIhZwFpxXtnHjX5VDLtl",
-        "Que esta passant entre Polonia i Bielorrusia? Pablo Casado ha gastat la porta magica de Doraemon i s.ha teletransportat involuntariament a la misa en honor a Franco! Els pijos madrilenys ataquen de nou i Esperanza Gracia ens dona la bona nit per Twitter.",
-        1,
-        CURRENT_TIMESTAMP,
-        CURRENT_TIMESTAMP
-    ),
-    (
-        null,
-        "1X00: The first dance",
-        "https://open.spotify.com/episode/5aViCTajwjdlwug3YnZo8X",
-        "Bienvenidos a DesVARiando, un programa en el que se analiza el fútbol de manera rigurosa, o quizás no tanto...",
-        2,
-        CURRENT_TIMESTAMP,
-        CURRENT_TIMESTAMP
-    ),
-    (
-        null,
-        "1X01: Juez de línea? Seguro que le gustaban mucho las rayas",
-        "https://open.spotify.com/episode/5bVK8uoeMxTpoxm7BuzRv7",
-        "En nuestro primer programa, hablamos del VAR, un tema que nos inspiró para nuestro nombre, y por ello, era obligatorio que se hablará de ello para empezar. Además hablamos también del arbitraje en la actualidad y en Europa. Para terminar, Jero nos trae un juego sobre árbitros que nada tiene que ver con el Indicius de Colgados del Aro.",
-        2,
         CURRENT_TIMESTAMP,
         CURRENT_TIMESTAMP
     );
+*/
